030e30c1071f67c9b17779bc5d06ac308c4dfdb2
Loïc Estève <loic.esteve@ymail.com>
2025-10-03 15:50:24 +0200

    fetch data from empty cache + multiple attempts + timing

--- fetch.py
+++ fetch.py
@@ -1,10 +1,23 @@
+import time
+from pathlib import Path
+import shutil
+
 from sklearn import datasets
 
 fetcher_names = [
     each for each in dir(datasets) if each.startswith("fetch_") and not ("openml" in each or "file" in each)
 ]
 
+data_home = Path(datasets.get_data_home())
+
+if data_home.exists():
+    shutil.rmtree(data_home)
+
 for name in fetcher_names:
     print(f"{name=}")
+    tic = time.time()
     fetcher = getattr(datasets, name)
     fetcher()
+    elapsed = time.time() - tic
+    print(f"{elapsed=}")
+
